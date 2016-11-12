f0bed799958f548a0e51ebc0c4243354da71708d
Aakash Tyagi <aakashtyagi@Aakashs-MacBook-Pro.local>
2016-11-11 23:40:10 -0600

    greetFirstTimers() postback test

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.post('/webhook', function (req, res) {
 		    }
 		} else if (event.postback) {
 		    console.log("Postback received: " + JSON.stringify(event.postback));
+		    console.log("Payload value received: " + JSON.stringify(event.postback.payload));
 		    if (event.postback.payload == "get started" || event.postback.payload == "Get Started"){
 		    	console.log("User just pressed get started. Info: New User incoming.");
 		    }
