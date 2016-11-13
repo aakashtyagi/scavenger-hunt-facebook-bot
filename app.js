41aeeac6d8d39a8a187349eefb7628144ba83d63
Aakash Tyagi <aakashtyagi@Aakashs-MacBook-Pro.local>
2016-11-13 11:30:44 -0600

    verifying loc as attachment

--- app.js
+++ app.js
@@ -36,18 +36,27 @@ app.get('/webhook', function (req, res) {
 // handler receiving messages
 app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
-    if(events.message.hasOwnProperty("attachments")){
-	    if(typeof events.message.attachments[0] !== undefined){
-	    	if(events.message.attachments[0].hasOwnProperty("payload")){
-					if(typeof events.message.attachments[0].payload.coordinates !== undefined){
-						console.log("ITS WORKING BITCHES!!!");
-						console.log(events.message.attachments[0].payload.coordinates.lat);
-						console.log(events.message.attachments[0].payload.coordinates.long);
-					}
+
+    if(events.hasOwnProperty("message")){
+    	if(events.message.hasOwnProperty("attachments")){
+    		if(events.message.attachments[0].hasOwnProperty("payload")){
+    			console.log("fuck off");
+    		}
+    	}
+    }
+
+  //   if(events.message.hasOwnProperty("attachments")){
+	 //    if(typeof events.message.attachments[0] !== undefined){
+	 //    	if(events.message.attachments[0].hasOwnProperty("payload")){
+		// 			if(typeof events.message.attachments[0].payload.coordinates !== undefined){
+		// 				console.log("ITS WORKING BITCHES!!!");
+		// 				console.log(events.message.attachments[0].payload.coordinates.lat);
+		// 				console.log(events.message.attachments[0].payload.coordinates.long);
+		// 			}
 					
-				}
-			}
-		}
+		// 		}
+		// 	}
+		// }
     for (i = 0; i < events.length; i++) {
         var event = events[i];
         if (event.message && event.message.text) {
