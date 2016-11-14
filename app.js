9fd36c35401ac08a1e047d5130c058838c5872db
Aakash Tyagi <aakashtyagi@Aakashs-MacBook-Pro.local>
2016-11-13 22:15:30 -0600

    user personal info

--- app.js
+++ app.js
@@ -196,19 +196,19 @@ function getUserDetails(recipientId){
     callback = function(response) {
 	  str = '';
 
-	  //another chunk of data has been recieved, so append it to `str`
+	  //another chunk of data has been received, so append it to `str`
 	  response.on('data', function (chunk) {
 	    str += chunk;
 	  });
 
-	  //the whole response has been recieved, so we just print it out here
+	  //the whole response has been received, so we just print it out here
 	  response.on('end', function () {
 	    console.log(str);
+	    return str;
 	  });
 	}
 
 	http.request(options, callback).end();
-	return str;
 }
 
 // send message on get started with options to choose city
